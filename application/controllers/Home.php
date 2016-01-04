74ccf205dab97579884e1679c806e6a47fb94269
bhmn91@gmail.com <breno23>
2016-01-04 15:07:32 -0200

    versao estavel

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,19 +5,19 @@ class Home extends MY_Controller
         public function __construct() 
         {
                 parent::__construct(FALSE);
-                $this->load->model(array('requests_model'));
+                $this->load->model(array('requests_model', 'users_model'));
         }
 
         public function index()
         {
-                $dados = array();
+                $data = $this->get_itens_table();
                 $this->layout
                         ->set_title('Faz, Que Falta')
                         ->set_keywords('Faz, Que Falta')
                         ->set_description('Faz, Que Falta, o sistema que ajuda na melhoria de seu bairro.')
                         ->set_includes('js/chart/Chart.js')
                         ->set_includes('js/home.js')
-                        ->set_view('site/home', $dados);
+                        ->set_view('site/home', $data);
         }
         
         public function get_charts()
@@ -27,5 +27,14 @@ class Home extends MY_Controller
                 $charts['citys'] = $this->requests_model->get_itens_by_city();
                 
                 echo (empty($charts['type_business']) || empty($charts['neighborhood']) || empty($charts['citys'])) ? 0 : json_encode($charts);
+        }
+        
+        public function get_itens_table()
+        {
+                $table['all_requests'] = $this->requests_model->get_total_itens();
+                $table['businessman'] = $this->users_model->get_total_itens('ctp_users.id_type_user = 2');
+                $table['citizens'] = $this->users_model->get_total_itens('ctp_users.id_type_user <> 3 AND ctp_users.id_type_user <> 2');
+                $table['open_requests'] = $this->requests_model->get_total_itens('ctp_requests.id_type_request_status = 1');
+                return $table;
         }
 }
