273eea7e83f8a2274f1932b3ad2011ca1256491d
bhmn91@gmail.com <breno23>
2016-05-03 16:36:45 -0300

    correcoes

--- application/views/pages/admin/contents/files.php
+++ application/views/pages/admin/contents/files.php
@@ -5,7 +5,7 @@
     </header>
     <div class="margin-left-20 margin-top-20">
         <div class="margin-bottom-10">
-            <a href="<?php echo $action_adicionar; ?>" class="btn btn-3d btn-blue"><i class="fa fa-files-o"></i>Importar</a>
+            <!--<a href="<?php //echo $action_adicionar; ?>" class="btn btn-3d btn-blue"><i class="fa fa-files-o"></i>Importar</a>-->
             <button id="btn-done" class="btn btn-3d btn-green"><i class="fa fa-check"></i>Concluir</button>
             <button id="btn-delete" class="btn btn-3d btn-red"><i class="fa fa-close"></i>Excluir</button>
         </div>
